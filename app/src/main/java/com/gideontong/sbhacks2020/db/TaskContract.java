39655f770601000cff76aaca853cc40667899345
Gideon Tong <gideon@gideontong.com>
2020-01-10 21:46:40 -0800

    Add TaskContract class with statics

---
+++ app/src/main/java/com/gideontong/sbhacks2020/db/TaskContract.java
@@ -0,0 +1,8 @@
+package com.gideontong.sbhacks2020.db;
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final string DB_NAME = "com.gideontong.sbhacks2020.db";
+    public static final int DB_VERSION = 1;
+}
