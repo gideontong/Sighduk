1d603a5c20393630c6def0733a29ff840b98ee3c
Gideon Tong <gideon@gideontong.com>
2020-01-11 13:23:58 -0800

    Copy SubjectData class

--- app/src/main/java/com/gideontong/sighduk/EntryData.java
+++ app/src/main/java/com/gideontong/sighduk/EntryData.java
@@ -1,4 +1,12 @@
 package com.gideontong.sighduk;
 
-public class EntryData {
+class SubjectData {
+    String SubjectName;
+    String Link;
+    String Image;
+    public SubjectData(String subjectName, String link, String image) {
+        this.SubjectName = subjectName;
+        this.Link = link;
+        this.Image = image;
+    }
 }
