2f7c2dd66a08f6a406565c93f1481bce0e883c82
Gideon Tong <gideon@gideontong.com>
2020-01-10 23:56:04 -0800

    Create a GetRequest function from the internet

--- app/src/main/java/com/gideontong/sbhacks2020/search/Networking.java
+++ app/src/main/java/com/gideontong/sbhacks2020/search/Networking.java
@@ -1,4 +1,26 @@
 package com.gideontong.sbhacks2020.search;
 
 public class Networking {
+    public static void GetRequest() throws IOException {
+        URL urlForGetRequest = new URL("https://jsonplaceholder.typicode.com/posts/1");
+        String readLine = null;
+        HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
+        conection.setRequestMethod("GET");
+        conection.setRequestProperty("userId", "a1bcdef"); // set userId its a sample here
+        int responseCode = conection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(conection.getInputStream()));
+            StringBuffer response = new StringBuffer();
+            while ((readLine = in.readLine()) != null) {
+                response.append(readLine);
+            }
+            in.close();
+            // print result
+            System.out.println("JSON String Result " + response.toString());
+            //GetAndPost.POSTRequest(response.toString());
+        } else {
+            System.out.println("GET NOT WORKED");
+        }
+    }
 }
