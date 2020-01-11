98c945839c9cb006a4794c7e08daf0026c7b34df
lwon97 <lwon1@uci.edu>
2020-01-11 03:29:03 -0800

    Updated search_item

--- app/src/main/java/com/gideontong/sbhacks2020/search_item.java
+++ app/src/main/java/com/gideontong/sbhacks2020/search_item.java
@@ -6,9 +6,22 @@ import android.os.Bundle;
 
 public class search_item extends AppCompatActivity {
 
+    /*
+    private Button search_button;
+    private EditText search_text;
+
+    private RecyclerView search_results;
+    private ArrayList<String> listOfItems;
+    private ItemAdapter itemAdapter;
+
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_item);
+
+        setContentView(R.layout.activity_search_item);
+
     }
 }
