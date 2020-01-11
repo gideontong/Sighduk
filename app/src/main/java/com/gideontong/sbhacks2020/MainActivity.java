1eb63e938b9ddf7aba35c7bab8cb64da78a15923
Gideon Tong <gideon@gideontong.com>
2020-01-10 22:19:59 -0800

    Removed unused import statements

--- app/src/main/java/com/gideontong/sbhacks2020/MainActivity.java
+++ app/src/main/java/com/gideontong/sbhacks2020/MainActivity.java
@@ -8,13 +8,9 @@ import android.os.Bundle;
 
 import com.gideontong.sbhacks2020.db.ShowContract;
 import com.gideontong.sbhacks2020.db.ShowDbHelper;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
