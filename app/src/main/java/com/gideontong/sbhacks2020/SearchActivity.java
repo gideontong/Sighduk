3748a5fdc03c2f33207ca48778fcdbab97b166b9
Gideon Tong <gideon@gideontong.com>
2020-01-11 01:06:35 -0800

    Remove unnecessary deps

--- app/src/main/java/com/gideontong/sbhacks2020/SearchActivity.java
+++ app/src/main/java/com/gideontong/sbhacks2020/SearchActivity.java
@@ -1,7 +1,5 @@
 package com.gideontong.sbhacks2020;
 
-import android.app.AppComponentFactory;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchActivity extends AppCompatActivity {
