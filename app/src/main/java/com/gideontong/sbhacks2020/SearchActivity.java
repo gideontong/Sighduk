5a06591522496fb9c435a8c3770655cd5b6120d4
Gideon Tong <gideon@gideontong.com>
2020-01-11 03:02:28 -0800

    Place DownloadFilesTask in SearchActivity

--- app/src/main/java/com/gideontong/sbhacks2020/SearchActivity.java
+++ app/src/main/java/com/gideontong/sbhacks2020/SearchActivity.java
@@ -40,4 +40,29 @@ public class SearchActivity extends AppCompatActivity {
         }
         Log.d(TAG, "EOL");
     }
+
+    private class DownloadFilesTask extends AsyncTask<URL, Integer, Long> {
+        // Do the long-running work in here
+        protected Long doInBackground(URL... urls) {
+            int count = urls.length;
+            long totalSize = 0;
+            for (int i = 0; i < count; i++) {
+                totalSize += Downloader.downloadFile(urls[i]);
+                publishProgress((int) ((i / (float) count) * 100));
+                // Escape early if cancel() is called
+                if (isCancelled()) break;
+            }
+            return totalSize;
+        }
+
+        // This is called each time you call publishProgress()
+        protected void onProgressUpdate(Integer... progress) {
+            setProgressPercent(progress[0]);
+        }
+
+        // This is called when doInBackground() is finished
+        protected void onPostExecute(Long result) {
+            showNotification("Downloaded " + result + " bytes");
+        }
+    }
 }
