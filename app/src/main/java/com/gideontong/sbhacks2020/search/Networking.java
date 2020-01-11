f0c29e238ba7ff1bbe83e8ea514f11cfce75a19d
Gideon Tong <gideon@gideontong.com>
2020-01-11 00:01:03 -0800

    Copied POST request code

--- app/src/main/java/com/gideontong/sbhacks2020/search/Networking.java
+++ app/src/main/java/com/gideontong/sbhacks2020/search/Networking.java
@@ -29,4 +29,39 @@ public class Networking {
             System.out.println("GET request failed with response code " + responseCode);
         }
     }
+
+    public static void POSTRequest() throws IOException {
+        final String POST_PARAMS = "{\n" + "\"userId\": 101,\r\n" +
+                "    \"id\": 101,\r\n" +
+                "    \"title\": \"Test Title\",\r\n" +
+                "    \"body\": \"Test Body\"" + "\n}";
+        System.out.println(POST_PARAMS);
+        URL obj = new URL("https://jsonplaceholder.typicode.com/posts");
+        HttpURLConnection postConnection = (HttpURLConnection) obj.openConnection();
+        postConnection.setRequestMethod("POST");
+        postConnection.setRequestProperty("userId", "a1bcdefgh");
+        postConnection.setRequestProperty("Content-Type", "application/json");
+        postConnection.setDoOutput(true);
+        OutputStream os = postConnection.getOutputStream();
+        os.write(POST_PARAMS.getBytes());
+        os.flush();
+        os.close();
+        int responseCode = postConnection.getResponseCode();
+        System.out.println("POST Response Code :  " + responseCode);
+        System.out.println("POST Response Message : " + postConnection.getResponseMessage());
+        if (responseCode == HttpURLConnection.HTTP_CREATED) { //success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    postConnection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("POST NOT WORKED");
+        }
+    }
 }
