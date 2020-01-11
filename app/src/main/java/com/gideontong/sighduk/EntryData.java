0cddd57c4480b979e9712f1b1954214234aadd7f
Gideon Tong <gideon@gideontong.com>
2020-01-11 13:24:22 -0800

    Update SubjectData to EntryData constructor

--- app/src/main/java/com/gideontong/sighduk/EntryData.java
+++ app/src/main/java/com/gideontong/sighduk/EntryData.java
@@ -4,7 +4,7 @@ class EntryData {
     String SubjectName;
     String Link;
     String Image;
-    public SubjectData(String subjectName, String link, String image) {
+    public EntryData(String subjectName, String link, String image) {
         this.SubjectName = subjectName;
         this.Link = link;
         this.Image = image;
