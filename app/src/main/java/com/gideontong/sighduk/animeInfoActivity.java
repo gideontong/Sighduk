e6000883e3444b9ef6165fe8df07b5ff2fe44120
Brian Lam <brilam8@gmail.com>
2020-01-11 15:08:00 -0800

    linked views

--- app/src/main/java/com/gideontong/sighduk/animeInfoActivity.java
+++ app/src/main/java/com/gideontong/sighduk/animeInfoActivity.java
@@ -1,11 +1,17 @@
 package com.gideontong.sighduk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.gideontong.sighduk.db.ShowDbHelper;
+
+import org.w3c.dom.Text;
 
 public class animeInfoActivity extends AppCompatActivity {
 
@@ -14,5 +20,14 @@ public class animeInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.anime_show_info);
 
+        TextView animeTitle = findViewById(R.id.animeTitle);
+        TextView animeScore = findViewById(R.id.scoreValue);
+        TextView animeRank = findViewById(R.id.rankValue);
+        TextView synopsisText = findViewById(R.id.synopsisText);
+        ImageView animeImage = findViewById(R.id.animeImage);
+        Button addWatchList = findViewById(R.id.addWatchList);
+
+        Intent intent = new Intent(animeInfoActivity.this, search_item.class);
+        startActivity(intent);
     }
 }
