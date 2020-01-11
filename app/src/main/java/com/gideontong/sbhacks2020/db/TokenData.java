cf0821a167965784aeb01b2b75e948da6f6231c1
Gideon Tong <gideon@gideontong.com>
2020-01-11 00:30:41 -0800

    Create TokenData for DB storage

---
+++ app/src/main/java/com/gideontong/sbhacks2020/db/TokenData.java
@@ -0,0 +1,6 @@
+package com.gideontong.sbhacks2020.db;
+
+public class TokenData {
+    public static final String DB_NAME = "com.gideontong.sbhacks2020.db";
+    public static final int DB_VERSION = 1;
+}
