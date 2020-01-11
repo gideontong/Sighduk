205514071f23bd593dbf4f5a43f7acbb85ae4033
Gideon Tong <gideon@gideontong.com>
2020-01-10 21:50:31 -0800

    Create a constructor and imports for ShowDbHelper

--- app/src/main/java/com/gideontong/sbhacks2020/db/ShowDbHelper.java
+++ app/src/main/java/com/gideontong/sbhacks2020/db/ShowDbHelper.java
@@ -1,4 +1,9 @@
 package com.gideontong.sbhacks2020.db;
 
+import android.content.Context;
+
 public class ShowDbHelper {
+    public TaskDbHelper(Context context) {
+        super(context, ShowContract.DB_NAME, null, ShowContract.DB_VERSION);
+    }
 }
