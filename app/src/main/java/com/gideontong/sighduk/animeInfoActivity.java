d808b3338d21fa8efd206c5bca73420d8aa056fe
Brian Lam <brilam8@gmail.com>
2020-01-11 14:49:24 -0800

    starting anime information activity class

---
+++ app/src/main/java/com/gideontong/sighduk/animeInfoActivity.java
@@ -0,0 +1,18 @@
+package com.gideontong.sighduk;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gideontong.sighduk.db.ShowDbHelper;
+
+public class animeInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.anime_show_info);
+
+    }
+}
