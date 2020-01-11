5c371cac617a250a874a9e350f70b9f0e8d6fd4d
Gideon Tong <gideon@gideontong.com>
2020-01-11 09:53:44 -0800

    Add super.onCreate in TermsActivity

--- app/src/main/java/com/gideontong/sbhacks2020/TermsActivity.java
+++ app/src/main/java/com/gideontong/sbhacks2020/TermsActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class TermsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
