7df01bfb9cf03b312261894a086b0d6fe4644208
Gideon Tong <gideon@gideontong.com>
2020-01-11 11:41:36 -0800

    Add deps to MainActivity

--- app/src/main/java/com/gideontong/sbhacks2020/MainActivity.java
+++ app/src/main/java/com/gideontong/sbhacks2020/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.gideontong.sbhacks2020.db.ShowContract;
