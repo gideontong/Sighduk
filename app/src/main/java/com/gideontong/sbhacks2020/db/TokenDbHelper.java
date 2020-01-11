3bda4921aef50f8bb23b56886b7104004b0c857d
Gideon Tong <gideon@gideontong.com>
2020-01-11 00:32:32 -0800

    Create TokenDbHelper constructor

--- app/src/main/java/com/gideontong/sbhacks2020/db/TokenDbHelper.java
+++ app/src/main/java/com/gideontong/sbhacks2020/db/TokenDbHelper.java
@@ -3,4 +3,6 @@ package com.gideontong.sbhacks2020.db;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class TokenDbHelper extends SQLiteOpenHelper {
+    public TokenDbHelper(Context contet) {
+    }
 }
