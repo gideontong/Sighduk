3287723a8370d685969684c28e053301e8b87630
Gideon Tong <gideon@gideontong.com>
2020-01-11 09:50:35 -0800

    Create onCreateOptionsMenu in TermsActivity

--- app/src/main/java/com/gideontong/sbhacks2020/TermsActivity.java
+++ app/src/main/java/com/gideontong/sbhacks2020/TermsActivity.java
@@ -1,13 +1,17 @@
 package com.gideontong.sbhacks2020;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TermsActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
     }
     // This needs to be a screen for terms and conditions
 
