b9aa321e44cb82f7da65a2086cdb7f2a89f01e8a
Gideon Tong <gideon@gideontong.com>
2020-01-11 00:36:03 -0800

    Override onCreate function in TokenDbHelper

--- app/src/main/java/com/gideontong/sbhacks2020/db/TokenDbHelper.java
+++ app/src/main/java/com/gideontong/sbhacks2020/db/TokenDbHelper.java
@@ -1,10 +1,15 @@
 package com.gideontong.sbhacks2020.db;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class TokenDbHelper extends SQLiteOpenHelper {
     public TokenDbHelper(Context context) {
         super(context, TokenData.DB_NAME, null, TokenData.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
     }
 }
