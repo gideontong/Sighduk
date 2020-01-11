6fe445f211cfd5f0b664714fb1f275de08a71446
Gideon Tong <gideon@gideontong.com>
2020-01-11 01:13:57 -0800

    Set content view of SearchActivity

--- app/src/main/java/com/gideontong/sbhacks2020/SearchActivity.java
+++ app/src/main/java/com/gideontong/sbhacks2020/SearchActivity.java
@@ -10,5 +10,6 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
     }
 }
