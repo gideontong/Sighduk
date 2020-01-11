80359cb1883340415ba3615187923514af2b17b7
Gideon Tong <gideon@gideontong.com>
2020-01-10 21:51:17 -0800

    Correct the name of the ShowDbHelper constructor

--- app/src/main/java/com/gideontong/sbhacks2020/db/ShowDbHelper.java
+++ app/src/main/java/com/gideontong/sbhacks2020/db/ShowDbHelper.java
@@ -1,9 +1,11 @@
 package com.gideontong.sbhacks2020.db;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class ShowDbHelper {
-    public TaskDbHelper(Context context) {
+public class ShowDbHelper extends SQLiteOpenHelper {
+    public ShowDbHelper(Context context) {
         super(context, ShowContract.DB_NAME, null, ShowContract.DB_VERSION);
     }
 }
