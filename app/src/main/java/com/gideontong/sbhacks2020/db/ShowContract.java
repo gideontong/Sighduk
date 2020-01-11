d14fe9f721492ef6e29897cda5261b8fa0ffc597
Gideon Tong <gideon@gideontong.com>
2020-01-10 21:47:36 -0800

    Correct variable names and add ShowEntry class

--- app/src/main/java/com/gideontong/sbhacks2020/db/ShowContract.java
+++ app/src/main/java/com/gideontong/sbhacks2020/db/ShowContract.java
@@ -3,6 +3,11 @@ package com.gideontong.sbhacks2020.db;
 import android.provider.BaseColumns;
 
 public class TaskContract {
-    public static final string DB_NAME = "com.gideontong.sbhacks2020.db";
+    public static final String DB_NAME = "com.gideontong.sbhacks2020.db";
     public static final int DB_VERSION = 1;
+
+    public class ShowEntry implements BaseColumns {
+        public static final String TABLE = "shows";
+        public static final String COL_TASK_TITLE = "title";
+    }
 }
