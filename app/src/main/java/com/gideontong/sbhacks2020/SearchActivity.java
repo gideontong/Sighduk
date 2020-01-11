84e37635dadeb727ac81a82a90be90e6a4c69add
Gideon Tong <gideon@gideontong.com>
2020-01-11 01:09:41 -0800

    Tag SearchActivity class

--- app/src/main/java/com/gideontong/sbhacks2020/SearchActivity.java
+++ app/src/main/java/com/gideontong/sbhacks2020/SearchActivity.java
@@ -3,4 +3,5 @@ package com.gideontong.sbhacks2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchActivity extends AppCompatActivity {
+    public static final String TAG = "SearchActivity";
 }
