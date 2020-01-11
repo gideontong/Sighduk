6353dccbeba86ce3c5f8e92e05c1fcccd2426841
Gideon Tong <gideon@gideontong.com>
2020-01-11 01:10:52 -0800

    Replace onCreate in SearchActivity

--- app/src/main/java/com/gideontong/sbhacks2020/SearchActivity.java
+++ app/src/main/java/com/gideontong/sbhacks2020/SearchActivity.java
@@ -1,7 +1,13 @@
 package com.gideontong.sbhacks2020;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchActivity extends AppCompatActivity {
     public static final String TAG = "SearchActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    }
 }
