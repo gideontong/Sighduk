671909ff7dd46828bead01d01fd235d6364fb5f6
Gideon Tong <gideon@gideontong.com>
2020-01-11 01:11:08 -0800

    Call super constructor

--- app/src/main/java/com/gideontong/sbhacks2020/SearchActivity.java
+++ app/src/main/java/com/gideontong/sbhacks2020/SearchActivity.java
@@ -9,5 +9,6 @@ public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 }
