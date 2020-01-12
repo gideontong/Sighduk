b4aa153486324e0f4607842119958c4252086ec0
Gideon Tong <gideon@gideontong.com>
2020-01-11 18:16:35 -0800

    Add Fragment library

--- app/src/main/java/com/gideontong/sighduk/WatchlistFragment.java
+++ app/src/main/java/com/gideontong/sighduk/WatchlistFragment.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 public class FirstFragment extends Fragment {
 
     public FirstFragment() {}
