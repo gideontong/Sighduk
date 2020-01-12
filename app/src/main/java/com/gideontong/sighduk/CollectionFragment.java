20e1c49a17b832e24a68921fb88aec68353a438b
Gideon Tong <gideon@gideontong.com>
2020-01-11 17:32:29 -0800

    Corrected R.layout in CollectionFragment

--- app/src/main/java/com/gideontong/sighduk/CollectionFragment.java
+++ app/src/main/java/com/gideontong/sighduk/CollectionFragment.java
@@ -21,7 +21,7 @@ public class CollectionFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.collection_demo, container, false);
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
     @Override
