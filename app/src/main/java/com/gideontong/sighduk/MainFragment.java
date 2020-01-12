e8e19f96ed8284ee6d6dc2ab5f752baa0ed868e8
Gideon Tong <gideon@gideontong.com>
2020-01-11 17:19:26 -0800

    Replace layout with real layout

--- app/src/main/java/com/gideontong/sighduk/MainFragment.java
+++ app/src/main/java/com/gideontong/sighduk/MainFragment.java
@@ -18,7 +18,7 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_collection_object, container, false);
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
     @Override
