64c4a1a641f54ee7b3c64e08d4a45bf00ea71bc2
Gideon Tong <gideon@gideontong.com>
2020-01-11 19:37:24 -0800

    Copy code

--- app/src/main/java/com/gideontong/sighduk/WatchlistFragment.java
+++ app/src/main/java/com/gideontong/sighduk/WatchlistFragment.java
@@ -4,16 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
+import com.gideontong.sighduk.db.ShowDbHelper;
+
 public class WatchlistFragment extends Fragment {
+
+    private static final String TAG = "WatchlistFragment";
+
+    private ShowDbHelper mHelper;
+    private ListView mShowListView;
 
     public WatchlistFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watchlist);
+
+        mHelper = new ShowDbHelper(this);
+
+        mShowListView = findViewById(R.id.list_show);
+        updateUI();
     }
 
     @Override
