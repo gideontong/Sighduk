f8b8edf002c653dd87ec6650b7658c0ec5303cba
Gideon Tong <gideon@gideontong.com>
2020-01-12 05:53:38 -0800

    Override several functions in HistoryFragment

--- app/src/main/java/com/gideontong/sighduk/HistoryFragment.java
+++ app/src/main/java/com/gideontong/sighduk/HistoryFragment.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
+import com.gideontong.sighduk.db.SavedDbHelper;
+
 public class HistoryFragment extends Fragment {
+
+    public static final String TAG = "HistoryFragment";
+
+    private SavedDbHelper kHelper;
+    private ListView kShowListView;
 
     public HistoryFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        kHelper = new SavedDbHelper(getContext());
     }
 
     @Override
@@ -21,5 +30,11 @@ public class HistoryFragment extends Fragment {
                              ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // getView()
     }
 }
