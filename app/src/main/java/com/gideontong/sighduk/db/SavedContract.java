7eadac1b7b64d23e55e760e714e30fc10c2a6139
Gideon Tong <gideon@gideontong.com>
2020-01-12 05:33:30 -0800

    Create SavedEntry class

--- app/src/main/java/com/gideontong/sighduk/db/SavedContract.java
+++ app/src/main/java/com/gideontong/sighduk/db/SavedContract.java
@@ -3,4 +3,7 @@ package com.gideontong.sighduk.db;
 public class SavedContract {
     public static final String DB_NAME = "com.gideontong.sighduk.db.saved";
     public static final int DB_VERSION = 1;
+
+    public class SavedEntry {
+    }
 }
