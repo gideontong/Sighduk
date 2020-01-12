f6d264c64a48fb77c2d32bbccaa9c31cfd6c2d84
Gideon Tong <gideon@gideontong.com>
2020-01-11 17:02:22 -0800

    Copy onCreate in MainActivity from WatchlistActivity

--- app/src/main/java/com/gideontong/sighduk/MainActivity.java
+++ app/src/main/java/com/gideontong/sighduk/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -18,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivty";
 
     private ShowDbHelper mHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHelper = new ShowDbHelper(this);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
