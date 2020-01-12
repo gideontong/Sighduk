517345553884770941cd6cb25e3f3a339a71fd94
Gideon Tong <gideon@gideontong.com>
2020-01-11 17:14:30 -0800

    Implement CollectionPagerAdapter superclass constructor

--- app/src/main/java/com/gideontong/sighduk/CollectionPagerAdapter.java
+++ app/src/main/java/com/gideontong/sighduk/CollectionPagerAdapter.java
@@ -1,6 +1,10 @@
 package com.gideontong.sighduk;
 
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
 public class CollectionPagerAdapter extends FragmentStatePagerAdapter {
+    public CollectionPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
 }
