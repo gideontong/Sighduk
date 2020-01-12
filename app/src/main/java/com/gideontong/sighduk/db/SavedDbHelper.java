5126bdd12577bc8ce7d5568156b23e60b827bfcd
Gideon Tong <gideon@gideontong.com>
2020-01-12 05:37:57 -0800

    Implement onCreate override in SavedDbHelper

--- app/src/main/java/com/gideontong/sighduk/db/SavedDbHelper.java
+++ app/src/main/java/com/gideontong/sighduk/db/SavedDbHelper.java
@@ -1,10 +1,21 @@
 package com.gideontong.sighduk.db;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class SavedDbHelper extends SQLiteOpenHelper {
     public SavedDbHelper(Context context) {
         super(context, SavedContract.DB_NAME, null, ShowContract.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + SavedContract.SavedEntry.TABLE + " ( " +
+                SavedContract.SavedEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                SavedContract.SavedEntry.COL_SHOW_TITLE + " TEXT NOT NULL, "  +
+                SavedContract.SavedEntry.COL_SHOW_IMAGE_URL + " TEXT NOT NULL);";
+
+        db.execSQL(createTable);
     }
 }
