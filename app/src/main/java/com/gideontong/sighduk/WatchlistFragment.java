2bc9e0c3732a9d585205735f6d99a3d2505c5812
Gideon Tong <gideon@gideontong.com>
2020-01-11 17:46:28 -0800

    Copy WatchlistFragment code

---
+++ app/src/main/java/com/gideontong/sighduk/WatchlistFragment.java
@@ -0,0 +1,23 @@
+package com.gideontong.sighduk;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FirstFragment extends Fragment {
+
+    public FirstFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+}
