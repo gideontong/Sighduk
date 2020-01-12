cf282437b7f59dd1b9482dbda3a2b810ee02dd97
Gideon Tong <gideon@gideontong.com>
2020-01-12 05:33:43 -0800

    Add BaseColumns implementation

--- app/src/main/java/com/gideontong/sighduk/db/SavedContract.java
+++ app/src/main/java/com/gideontong/sighduk/db/SavedContract.java
@@ -1,9 +1,11 @@
 package com.gideontong.sighduk.db;
 
+import android.provider.BaseColumns;
+
 public class SavedContract {
     public static final String DB_NAME = "com.gideontong.sighduk.db.saved";
     public static final int DB_VERSION = 1;
 
-    public class SavedEntry {
+    public class SavedEntry implements BaseColumns {
     }
 }
