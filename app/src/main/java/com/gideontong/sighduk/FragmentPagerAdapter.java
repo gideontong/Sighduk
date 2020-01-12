4929fd1aa96d7bf6f00aa4ac823db71e84b29c74
Gideon Tong <gideon@gideontong.com>
2020-01-11 18:59:50 -0800

    Add deps

--- app/src/main/java/com/gideontong/sighduk/FragmentPagerAdapter.java
+++ app/src/main/java/com/gideontong/sighduk/FragmentPagerAdapter.java
@@ -1,4 +1,30 @@
 package com.gideontong.sighduk;
 
-public class FragmentPagerAdapter {
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+
+    public MyFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new FirstFragment();
+            case 1:
+                return new SecondFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
 }
