0516cfaebd3879bf1745bcebad409ee168e7536a
Gideon Tong <gideon@gideontong.com>
2020-01-11 17:27:32 -0800

    Copy CollectionDemoFragment from Google Docs

--- app/src/main/java/com/gideontong/sighduk/CollectionFragment.java
+++ app/src/main/java/com/gideontong/sighduk/CollectionFragment.java
@@ -1,4 +1,23 @@
 package com.gideontong.sighduk;
 
-public class CollectionFragment {
+public class CollectionDemoFragment extends Fragment {
+    // When requested, this adapter returns a DemoObjectFragment,
+    // representing an object in the collection.
+    DemoCollectionPagerAdapter demoCollectionPagerAdapter;
+    ViewPager viewPager;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.collection_demo, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        demoCollectionPagerAdapter = new DemoCollectionPagerAdapter(getChildFragmentManager());
+        viewPager = view.findViewById(R.id.pager);
+        viewPager.setAdapter(demoCollectionPagerAdapter);
+    }
 }
