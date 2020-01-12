41969fe99fd0ca045472a891d8d9b9016fa6d5ce
Gideon Tong <gideon@gideontong.com>
2020-01-11 17:28:44 -0800

    Add android and androidx deps

--- app/src/main/java/com/gideontong/sighduk/CollectionFragment.java
+++ app/src/main/java/com/gideontong/sighduk/CollectionFragment.java
@@ -1,5 +1,12 @@
 package com.gideontong.sighduk;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
