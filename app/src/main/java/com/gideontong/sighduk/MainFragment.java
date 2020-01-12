638b2bcee7e3cdaf6820ee46b5dc56a9557e7510
Gideon Tong <gideon@gideontong.com>
2020-01-11 17:18:59 -0800

    Copy code from Android Developer docs

--- app/src/main/java/com/gideontong/sighduk/MainFragment.java
+++ app/src/main/java/com/gideontong/sighduk/MainFragment.java
@@ -1,6 +1,13 @@
 package com.gideontong.sighduk;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.gideontong.sighduk.db.ShowDbHelper;
@@ -8,11 +15,18 @@ import com.gideontong.sighduk.db.ShowDbHelper;
 public class MainFragment extends Fragment {
     public static final String TAG = "MainFragment";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_watchlist);
+    public static final String ARG_OBJECT = "object";
 
-        mHelper = new ShowDbHelper(this);
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_collection_object, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Bundle args = getArguments();
+        ((TextView) view.findViewById(android.R.id.text1))
+                .setText(Integer.toString(args.getInt(ARG_OBJECT)));
     }
 }
