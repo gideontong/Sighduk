aebb154c6765e48dedbd7ee9e95e90f2d4cd1d97
Gideon Tong <gideon@gideontong.com>
2020-01-12 05:35:31 -0800

    Add constructor to SavedDbHelper with Context support

--- app/src/main/java/com/gideontong/sighduk/db/SavedDbHelper.java
+++ app/src/main/java/com/gideontong/sighduk/db/SavedDbHelper.java
@@ -1,6 +1,9 @@
 package com.gideontong.sighduk.db;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class SavedDbHelper extends SQLiteOpenHelper {
+    public SavedDbHelper(Context context) {
+    }
 }
