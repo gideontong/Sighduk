5ed1efc100a13a6b9a5a7c6c4d4a4ccc8ac34daf
Gideon Tong <gideon@gideontong.com>
2020-01-11 16:59:37 -0800

    Create MainActivity TAG

--- app/src/main/java/com/gideontong/sighduk/MainActivity.java
+++ app/src/main/java/com/gideontong/sighduk/MainActivity.java
@@ -3,4 +3,5 @@ package com.gideontong.sighduk;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivty";
 }
