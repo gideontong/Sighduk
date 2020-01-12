deddd7f88be3fceec0a533a342e8695f2080657e
Gideon Tong <gideon@gideontong.com>
2020-01-11 19:01:00 -0800

    Paste code

--- app/src/main/java/com/gideontong/sighduk/MainActivity.java
+++ app/src/main/java/com/gideontong/sighduk/MainActivity.java
@@ -3,4 +3,22 @@ package com.gideontong.sighduk;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    MyFragmentPagerAdapter myFragmentPagerAdapter;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        setPagerAdapter();
+    }
+
+    private void setPagerAdapter(){
+        myFragmentPagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(myFragmentPagerAdapter);
+    }
 }
