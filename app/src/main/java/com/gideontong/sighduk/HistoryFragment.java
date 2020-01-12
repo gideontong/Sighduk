3ea3a8d5e5f40cc8e857545929d0db1cb39a9f95
Gideon Tong <gideon@gideontong.com>
2020-01-11 18:38:36 -0800

    Fix missing fragment XML

--- app/src/main/java/com/gideontong/sighduk/HistoryFragment.java
+++ app/src/main/java/com/gideontong/sighduk/HistoryFragment.java
@@ -20,6 +20,6 @@ public class HistoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return inflater.inflate(R.layout.fragment_history, container, false);
     }
 }
--- app/src/main/java/com/gideontong/sighduk/WatchlistFragment.java
+++ app/src/main/java/com/gideontong/sighduk/WatchlistFragment.java
@@ -20,6 +20,6 @@ public class WatchlistFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return inflater.inflate(R.layout.fragment_watchlist, container, false);
     }
 }
