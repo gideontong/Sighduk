6b044d76e451b06424ad34c2827f058aedc6bfce
Gideon Tong <gideon@gideontong.com>
2020-01-11 17:07:32 -0800

    Move onOptionsItemSelected back to WatchlistActivity

--- app/src/main/java/com/gideontong/sighduk/WatchlistActivity.java
+++ app/src/main/java/com/gideontong/sighduk/WatchlistActivity.java
@@ -1,5 +1,8 @@
 package com.gideontong.sighduk;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
@@ -10,11 +13,13 @@ import android.os.Bundle;
 import com.gideontong.sighduk.db.ShowContract;
 import com.gideontong.sighduk.db.ShowDbHelper;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -87,7 +92,54 @@ public class WatchlistActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch(item.getItemId()) {
+            case R.id.action_add_show:
+                Log.d(TAG, "Add a new show button was pressed!");
+
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new TV show or anime")
+                        .setMessage("What's the name?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String show = String.valueOf(taskEditText.getText());
+
+                                SQLiteDatabase db = mHelper.getWritableDatabase();
+                                ContentValues values = new ContentValues();
+
+                                values.put(ShowContract.ShowEntry.COL_SHOW_TITLE, show);
+                                db.insertWithOnConflict(ShowContract.ShowEntry.TABLE,
+                                        null,
+                                        values,
+                                        SQLiteDatabase.CONFLICT_REPLACE);
+                                db.close();
+                                // updateUI();
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
+
+                return true;
+
+            case R.id.action_search:
+                Intent intent = new Intent(this, SearchActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.searchBtn:
+                Intent searchIntent = new Intent(this, LocalSearchActivity.class);
+                startActivity(searchIntent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void deleteShow(View view) {
