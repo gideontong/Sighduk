1b098fcdc30e266efbf97b6c02286a5df5641cf2
Gideon Tong <gideon@gideontong.com>
2020-01-11 17:07:56 -0800

    Removed unused deps in MainActivity

--- app/src/main/java/com/gideontong/sighduk/MainActivity.java
+++ app/src/main/java/com/gideontong/sighduk/MainActivity.java
@@ -1,18 +1,8 @@
 package com.gideontong.sighduk;
 
-import android.content.ContentValues;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-import android.widget.EditText;
-
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.gideontong.sighduk.db.ShowContract;
 import com.gideontong.sighduk.db.ShowDbHelper;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,10 +16,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_watchlist);
 
         mHelper = new ShowDbHelper(this);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
     }
 }
